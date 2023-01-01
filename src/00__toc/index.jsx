36b170f030f822dd607e2414b6f542235f5edb8e
Leo Wong <leoelicos@gmail.com>
2023-01-01 16:59:27 +1100

    TOC and style

---
+++ src/00__toc/index.jsx
@@ -0,0 +1,22 @@
+import './style/toc.css'
+import apps from './data/apps.json'
+import { Link } from 'react-router-dom'
+
+const { list } = apps
+
+function Toc() {
+  return (
+    <section className='link-container'>
+      {list.map(({ name, link }) => (
+        <Link
+          key={name}
+          to={link}
+          className='link'>
+          {name.replace(' ', '\n')}
+        </Link>
+      ))}
+    </section>
+  )
+}
+
+export default Toc
