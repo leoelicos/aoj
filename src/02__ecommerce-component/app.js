7f5d8c26de35ad135bea756c6da944d3fa610b24
Leo Wong <leoelicos@gmail.com>
2023-01-03 01:09:03 +1100

    Implement ecommerce component

---
+++ src/02__ecommerce-component/app.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    name: 'French Fries with Ketchup',
+    price: 223,
+    image: '/images/02__ecommerce-component/plate__french-fries.png',
+    alt: 'French Fries',
+    count: 2
+  },
+  {
+    name: 'Salmon and Vegetables',
+    price: 512,
+    image: '/images/02__ecommerce-component/plate__salmon-vegetables.png',
+    alt: 'Salmon and Vegetables',
+    count: 0
+  },
+  {
+    name: 'Spaghetti Meat Sauce',
+    price: 782,
+    image: '/images/02__ecommerce-component/plate__spaghetti-meat-sauce.png',
+    alt: 'Spaghetti with Meat Sauce',
+    count: 0
+  },
+  {
+    name: 'Bacon, Eggs, and Toast',
+    price: 599,
+    image: '/images/02__ecommerce-component/plate__bacon-eggs.png',
+    alt: 'Bacon, Eggs, and Toast',
+    count: 0
+  },
+  {
+    name: 'Chicken Salad with Parmesan',
+    price: 698,
+    image: '/images/02__ecommerce-component/plate__chicken-salad.png',
+    alt: 'Chicken Salad with Parmesan',
+    count: 0
+  },
+  {
+    name: 'Fish Sticks and Fries',
+    price: 634,
+    image: '/images/02__ecommerce-component/plate__fish-sticks-fries.png',
+    alt: 'Fish Sticks and Fries',
+    count: 1
+  }
+]
