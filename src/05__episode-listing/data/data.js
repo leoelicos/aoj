7499eb91006c2020022d05642724485679269ccc
Leo Wong <leoelicos@gmail.com>
2023-01-07 14:53:27 +1100

    List of episodes

---
+++ src/05__episode-listing/data/data.js
@@ -0,0 +1,12 @@
+export default [
+  { id: 'episode-1', title: 'Trailer' },
+  { id: 'episode-2', title: 'James Q Quick Origin Story' },
+  { id: 'episode-3', title: "Amy Dutton's Origin Story" },
+  { id: 'episode-4', title: 'Starting a New Development Project' },
+  { id: 'episode-5', title: 'How Do you Start a New Design Project?' },
+  { id: 'episode-6', title: 'Freelancing (Part 1)' },
+  { id: 'episode-7', title: 'Freelancing (Part 2)' },
+  { id: 'episode-8', title: 'The Tech Behind jamesqquick.com' },
+  { id: 'episode-9', title: 'The Tech Behind SelfTeach.me' },
+  { id: 'episode-10', title: 'Tech Behind SelfTeach.me' } //
+]
