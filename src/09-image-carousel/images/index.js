f20bfc5f2ac5eaf2c16cfdaeacf6e2b2893ad956
Leo Wong <leoelicos@gmail.com>
2023-01-08 22:56:12 +1100

    Export all of the images as an array

---
+++ src/09-image-carousel/images/index.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    image: require('./dave-hoefler-okUIdo6NxGo-unsplash.jpg'),
+    caption: 'Photo by Dave Hoefler on Unsplash'
+  },
+  {
+    image: require('./sherman-yang-VBBGigIuaDY-unsplash.jpg'),
+    caption: 'Photo by Sherman Yang n Unsplash'
+  },
+  {
+    image: require('./jakob-owens-EwRM05V0VSI-unsplash.jpg'),
+    caption: 'Photo by Jakob Owens on Unsplash'
+  },
+  {
+    image: require('./finding-dan-dan-grinwis-O35rT6OytRo-unsplash.jpg'),
+    caption: 'Photo by Dan Grinwis on Unsplash'
+  },
+  {
+    image: require('./vincentiu-solomon-ln5drpv_ImI-unsplash.jpg'),
+    caption: 'Photo by Vincentiu Solomon on Unsplash'
+  },
+  {
+    image: require('./silas-baisch-Wn4ulyzVoD4-unsplash.jpg'),
+    caption: 'Photo by Silas Baisch on Unsplash'
+  },
+  {
+    image: require('./eugene-golovesov-EXdXp7Z4X4w-unsplash.jpg'),
+    caption: 'Photo by Eugene Golovesov on Unsplash'
+  },
+  {
+    image: require('./jennifer-reynolds-_8HI5xf4TkE-unsplash.jpg'),
+    caption: 'Photo by Jennifer reynolds on Unsplash'
+  },
+  {
+    image: require('./kellen-riggin-SIBOiXKg0Ds-unsplash.jpg'),
+    caption: 'Photo by Kellen Riggin on Unsplash'
+  },
+  {
+    image: require('./rafael-hoyos-weht-zhkAp8DGkxw-unsplash.jpg'),
+    caption: 'Photo by Rafael Hoyos on Unsplash'
+  },
+  {
+    image: require('./sonya-romanovska-wzdXhyi3AiM-unsplash.jpg'),
+    caption: 'Photo by Sonya Romanovska on Unsplash'
+  }
+]
