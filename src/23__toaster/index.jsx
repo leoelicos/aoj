b2c80431b15b96872b771ea4d195a3ed9d718d17
Leo Wong <leoelicos@gmail.com>
2023-01-14 19:13:11 +1100

    Implement Toaster with useEffect, useRef, useState

--- src/23__toaster/index.jsx
+++ src/23__toaster/index.jsx
@@ -1,17 +1,57 @@
+import { useEffect, useRef, useState } from 'react'
 import { ReactComponent as Arrow } from './images/arrow.svg'
 import { ReactComponent as Close } from './images/close.svg'
 
 import './style/styles.css'
 
 export default function Toaster() {
+  const [collapsed, setCollapsed] = useState(true)
+
+  const lastCollapse = useRef(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (lastCollapse.current === null) {
+        console.log('Popup - 15 seconds after loading')
+        lastCollapse.current = new Date().getTime()
+        setCollapsed(false)
+      }
+    }, 15000)
+  }, [])
+
+  const handleMouseMove = (e) => {
+    let posY = e.clientY
+    let targetY = e.target.clientHeight
+    let d = new Date().getTime()
+    let difference = Math.floor((d - lastCollapse.current) / 1000)
+    if (posY < targetY / 2) {
+      if (lastCollapse.current === null) {
+        console.log('Popup - moved first time')
+        setCollapsed(false)
+        lastCollapse.current = d
+      } else if (difference >= 15) {
+        console.log('Popup - 15 seconds since last popup')
+        setCollapsed(false)
+        lastCollapse.current = d
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove)
+  }, [])
+
   return (
     <div className='aoj-23'>
       <div className='body'>
-        <div className='toaster'>
+        <div className={`toaster ${collapsed ? 'collapsed' : ''}`}>
           <div className='close'>
             <button
               className='close-toaster'
-              id='closeToaster'>
+              id='closeToaster'
+              onClick={() => {
+                setCollapsed(true)
+              }}>
               <Close />
               &nbsp; Close
             </button>
